38ad3aca6fbde7bb3139a4ef1568de8797be5106
Arretu <amosdiegel@gmail.com>
2023-11-10 12:03:03 +0100

    Update autostart.js

---
+++ home/autostart.js
@@ -0,0 +1,46 @@
+/** @param {NS} ns */
+//import { nap } from "/shared/lib.js";
+/** @param {NS} ns */
+export async function main(ns) {
+
+	let home = 'home';
+	ns.killall(home);
+
+	//Delete Data Directory
+	const dataDir = ns.ls(home,"data/");
+	ns.tprint("Preparing to delete", dataDir.toString());
+
+	for(const file of dataDir) {
+	ns.rm(file);
+	}
+
+	await ns.sleep(2000);
+	ns.tprint("Data directory wiped, reparing to generate new static data.");
+
+	//Generate /data/static/allservers.txt
+	ns.exec('/scripts/spider.js', 'home');
+
+	await ns.sleep(5000);
+	ns.tprint("All remote servers identified. Parsing data to new lists.")
+
+	//Generates allnpc.txt, alltargets.txt, 
+	ns.exec("/scripts/parseServers.js", 'home');
+
+	await ns.sleep(5000);
+	ns.tprint("Static files generated. Initialising daemons.");
+
+	//Start parseDaemon to generate non-static lists. Pause to allow a cycle.
+	ns.exec("/daemons/parseDaemon.js", 'home');
+	await ns.sleep(5000);
+	ns.tprint("parseDaemon running. Preparing rootDaemon.");
+
+	//Start rootDaemon to nuke initial hosts.
+	ns.exec("/daemons/rootDaemon.js",home);
+	await ns.sleep(5000);
+	ns.tprint("rootDaemon running.");
+	
+
+	// Start earlygame deployerDaemon.
+
+	ns.exec("/daemons/earlyDeployDaemon.js",home);
+}
